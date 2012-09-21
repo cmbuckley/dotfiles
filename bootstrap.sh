9277c569825d749ada8afda44a227f88bd4a7109
Chris Buckley <chris@cmbuckley.co.uk>
2012-09-21 14:36:05 +0100

    logic fixes

--- bootstrap.sh
+++ bootstrap.sh
@@ -21,7 +21,7 @@ git pull || exit $?
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
     setup
 else
-    read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+    read -p "This may overwrite existing files in your home directory. Are you sure? [y/N] " -n 1
     echo
     if [[ $REPLY =~ ^[Yy]$ ]]; then
         setup
@@ -30,5 +30,5 @@ fi
 
 # post-setup
 unset setup
-source "$HOME/.bash_profile"
+[ -f "$HOME/.bash_profile" ] && source "$HOME/.bash_profile"
 cd "$OWD"
